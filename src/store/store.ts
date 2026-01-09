580a2650e0b6caf368c1361d9bd32bb84c397380
Ivan Dobroznai <i.dobroznai@gmail.com>
2026-01-09 19:56:08 +0100

    update store.ts: added 'counter'

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { feedbackSlice } from "store/redux/feedback/feedbackSlice"
+import { counterSlice } from "./redux/counter/counterSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(feedbackSlice)
+const rootReducer = combineSlices(counterSlice, feedbackSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
